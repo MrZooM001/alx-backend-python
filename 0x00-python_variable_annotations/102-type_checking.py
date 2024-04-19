d28e4eeabcd62d99a83bbac1291f6cc396e0f5fc
Hazem Magdy <hhazem0010@gmail.com>
2024-04-19 03:47:02 +0200

    fix advanced tasks 102

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 """12. Type Checking"""
-from typing import Tuple, List, Union
+from typing import Tuple, List, Union, TypeVar
 
 
-def zoom_array(lst: Tuple[int, ...], factor: Union[int, float] = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Validate variables types"""
     if not isinstance(factor, int):
         factor = int(factor)
 
-    zoomed_in: List[int] = [item for item in lst for i in range(factor)]
+    zoomed_in: List = [item for item in lst for i in range(factor)]
     return zoomed_in
 
 
-array: Tuple[int, ...] = (12, 72, 91)
+array: Tuple = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
