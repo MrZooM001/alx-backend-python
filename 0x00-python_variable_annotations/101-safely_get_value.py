e3697424de49d28cbfc0baa8621e8ed882bba57b
Hazem Magdy <hhazem0010@gmail.com>
2024-04-19 03:42:43 +0200

    advanced tasks

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
+"""11. More involved type annotations"""
 from typing import TypeVar, Mapping, Any, Union
 
-T = TypeVar("T")
+U = TypeVar("U")
 
 
 def safely_get_value(
-    dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    dct: Mapping, key: Any, default: Union[U, None] = None) -> Union[Any, U]:
+    """type annotations"""
     if key in dct:
         return dct[key]
     else:
--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
+"""12. Type Checking"""
 from typing import Tuple, List, Union
 
 
-def zoom_array(lst: Tuple, factor: Union[int, float] = 2) -> List:
+def zoom_array(lst: Tuple[int, ...], factor: Union[int, float] = 2) -> List[int]:
     if not isinstance(factor, int):
         factor = int(factor)
 
-    zoomed_in: List = [item for item in lst for i in range(factor)]
+    zoomed_in: List[int] = [item for item in lst for i in range(factor)]
     return zoomed_in
 
 
-array: Tuple = (12, 72, 91)
+array: Tuple[int, ...] = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
