e3f997dbdca9f366370a96a13ec8da51957d7ecf
Hazem Magdy <hhazem0010@gmail.com>
2024-04-27 01:15:38 +0300

    python async comprehension -fix annotation  all tasks

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ Module to demostrate an async comprehension.
 """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Coroutine that loops 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -3,22 +3,18 @@
 Module to demostrate an async comprehension.
 """
 import asyncio
+import time
 
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     Coroutine that executes async_comprehension() four times in parallel using asyncio.gather,
     measures the total runtime and return it.
     """
-    start_time = asyncio.get_event_loop().time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-    )
-    total_runtime = asyncio.get_event_loop().time() - start_time
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    total_runtime = time.time() - start_time
 
     return total_runtime
